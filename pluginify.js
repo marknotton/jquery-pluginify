ccebd2d5db592232724b3db997dc0c0a1fc03a0f
Mark Notton <mark@yellostudio.co.uk>
2018-06-04 09:58:14 +0100

    Removed default 'settings' merge. Opting for only using 'default' None iterated declarations now return the class object Added smarter warning messaging to avoid duplicate declarations on identical elements

--- pluginify.js
+++ pluginify.js
@@ -2,13 +2,7 @@
 // @see https://github.com/marknotton/jquery-pluginify
 **/
 
-function pluginify(...settings) {
-
-  if (settings.length == 1 && typeof settings == 'object') {
-    var {name, classname, iterate = true, shorthand = false} = settings[0];
-  } else {
-    var [name, classname, iterate = true, shorthand = false] = [settings][0];
-  }
+function pluginify(name, classname, iterate = true, shorthand = false) {
 
   if ( typeof name == 'undefined' || typeof classname == 'undefined' ) {
     console.warn('Pluginify requires a name and class reference');
@@ -17,19 +11,25 @@ function pluginify(...settings) {
 
   let dataName = `__${name}`;
   let old = $.fn[name];
+  let warned = false;
 
   $.fn[name] = function (...option) {
 
     if (iterate) {
 
-      this.each((index, element) => {
+      return this.each((index, element) => {
 
         let $this = $(element);
         let data = $this.data(dataName);
-        let options = $.extend({}, classname.settings, classname.defaults, $this.data(), typeof option === 'object' && option);
+        let options = $.extend({}, classname.defaults, $this.data(), typeof option === 'object' && option);
 
         if (!data) {
           $this.data(dataName, (data = new classname(this, options)));
+        } else {
+          if (!warned) {
+            console.warn($this[0],  " These items have already been set to iterate already");
+          }
+          warned = true;
         }
 
         if (typeof option === 'string') {
@@ -42,15 +42,22 @@ function pluginify(...settings) {
 
       let $this = $(this);
       let data = $this.data(dataName);
-      let options = $.extend({}, classname.settings, classname.defaults, $this.data(), typeof option === 'object' && option);
+      let options = $.extend({}, classname.defaults, $this.data(), typeof option === 'object' && option);
 
       if (!data) {
         $this.data(dataName, (data = new classname(this, options)));
+      } else {
+        if (!warned) {
+          console.warn($this[0],  " These items have already been set to iterate already");
+        }
+        warned = true;
       }
 
       if (typeof option === 'string') {
         data[option]();
       }
+
+      return data;
 
     }
 
